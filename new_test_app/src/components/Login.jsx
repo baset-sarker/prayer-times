0146666662b8ba7abc7b23b74e9fd254fae365a9
MAB Sarker <chayan2005@gmail.com>
2025-02-08 16:20:15 -0500

    udpaed login

--- new_test_app/src/components/Login.jsx
+++ new_test_app/src/components/Login.jsx
@@ -10,8 +10,8 @@ function Login({ onLogin }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const serverURL = import.meta.env.VITE_SERVER_URL; // Get the URL
-      const response = await axios.post(`${serverURL}/api/auth/login`, { username, password }); // Use it
+      // const serverURL = import.meta.env.VITE_SERVER_URL; // Get the URL
+      const response = await axios.post(`/api/auth/login`, { username, password }); // Use it
       onLogin(response.data.token);
     } catch (err) {
       setError(err.response?.data?.message || 'Invalid credentials');
@@ -19,18 +19,23 @@ function Login({ onLogin }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {error && <div className="alert alert-danger">{error}</div>}
-      <div className="mb-3">
-        <label htmlFor="username" className="form-label">Username</label>
-        <input type="text" className="form-control" id="username" onChange={(e) => setUsername(e.target.value)} />
+    <div className='container' style={{ minHeight: '1024px' }}>
+      <div className='col-md-6 offset-md-3'>
+      <form onSubmit={handleSubmit}>
+        {error && <div className="alert alert-danger">{error}</div>}
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">Username</label>
+          <input type="text" className="form-control" id="username" onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
       </div>
-      <div className="mb-3">
-        <label htmlFor="password" className="form-label">Password</label>
-        <input type="password" className="form-control" id="password" onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <button type="submit" className="btn btn-primary">Login</button>
-    </form>
+    </div>
+    
   );
 }
 
