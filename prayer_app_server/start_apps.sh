b4a8ed7ef5ec4a926d468b0e2f600d7eaaa5228a
MAB Sarker <chayan2005@gmail.com>
2025-02-11 11:14:53 -0500

    added start app sh

---
+++ prayer_app_server/start_apps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Delay before starting the apps (e.g., 10 seconds)
+sleep 10
+
+# Start Node.js app
+cd /home/pi/prayer-times/prayer_app_server
+npm start &
+
+# Delay before starting Electron app (e.g., 5 seconds)
+sleep 5
+
+# Start Electron app
+cd /home/pi/prayer-times/electron-prayer-app
+npm start &
