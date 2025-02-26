164f286f5c901c8acf8a60ff198e51eb56879d60
MAB Sarker <chayan2005@gmail.com>
2025-02-26 11:28:40 -0500

    updated image

--- new_test_app/src/components/Contact.jsx
+++ new_test_app/src/components/Contact.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Contact = () => {
   return (
-        <section className="page-section" id="contact">
+        <section className="page-section" id="contact" >
             <div className="container">
-                <div className="row my-5" >
+                <div className="row my-5">
                     <div className="col-lg-6  col-md-6  text-center">
                         <h2 className="section-heading text-uppercase">Contact</h2>
                         <h3 className="section-subheading text-white">
