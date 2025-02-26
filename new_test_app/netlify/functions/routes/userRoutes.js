89ec63d069d02903849a7db3133f5030be7ac02d
MAB Sarker <chayan2005@gmail.com>
2025-02-26 15:29:05 -0500

    added userlist removed pass

--- new_test_app/netlify/functions/routes/userRoutes.js
+++ new_test_app/netlify/functions/routes/userRoutes.js
@@ -40,7 +40,8 @@ router.post('/login', async (req, res) => {
 
 router.get('/',authenticateJWT, async (req, res) => {
   try {
-    const users = await User.find({})
+     // want to exclude password field
+    const users = await User.find({}, { password: 0 }) 
     res.json(users);
   } catch (err) {
     res.status(500).json({ message: err.message });
