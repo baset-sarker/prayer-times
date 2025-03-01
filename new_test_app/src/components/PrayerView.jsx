dcceef72242bf052ee6e276a7110ff83749a3089
MAB Sarker <chayan2005@gmail.com>
2025-02-28 22:52:15 -0500

    updated image and added updated at

--- new_test_app/src/components/PrayerView.jsx
+++ new_test_app/src/components/PrayerView.jsx
@@ -78,9 +78,6 @@ const PrayerView = () => {
         year: "numeric",
         month: "short",
         day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
         hour12: false,
       }).format(date);
     };
