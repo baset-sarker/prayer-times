6efcea572f17329bd115bd9798a9eb68e4055a51
MAB Sarker <chayan2005@gmail.com>
2025-03-13 22:21:10 -0400

    yt path change

--- new_test_app/src/components/PrayerView.jsx
+++ new_test_app/src/components/PrayerView.jsx
@@ -250,7 +250,7 @@ const PrayerView = () => {
                               <h2 className="text-white">Useful links</h2>
                               <p className="text-white text-center">Youtube channel, helps to memorize quran</p>
                               <a target='blank' style={{"text-decoration": "none"}} href='https://www.youtube.com/@Worldofpeace-vm4bc' className="text-white">
-                              <img height={'40px'} src='/public/youtube.webp'></img> &nbsp; World of peace</a>
+                              <img height={'40px'} src='youtube.webp'></img> &nbsp; World of peace</a>
                           </div>
                     </div>
 
