29bd847681c1b0f8bab613f869f060b5ab653b6c
MAB Sarker <chayan2005@gmail.com>
2025-03-17 01:04:34 -0400

    removed arabic in whatsapp added get iqama time probelm solved

--- new_test_app/src/components/WhatsAppShare.jsx
+++ new_test_app/src/components/WhatsAppShare.jsx
@@ -10,7 +10,7 @@ These are not prayer times, these are times we gather at our masjid for salat:
                                   
 Fajr Iqama: ${getIqaamahTime(prayer.fajr_api, prayer.fajr, prayer.fajr_added_time)}
 Dhuhr Iqama: ${getIqaamahTime(prayer.duhr_api, prayer.duhr, prayer.duhr_added_time)}
-Jummah Iqama: ${prayer.jummah}
+Jummah: ${prayer.jummah}
 Asr Iqama: ${getIqaamahTime(prayer.asr_api, prayer.asr, prayer.asr_added_time)}
 Magrib Iqama: ${getIqaamahTime(prayer.magrib_api, prayer.magrib, prayer.magrib_added_time)}
 Isha Iqama: ${getIqaamahTime(prayer.isha_api, prayer.isha, prayer.isha_added_time)}`;
