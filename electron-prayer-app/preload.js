740300f316c96b0a9386fd8e62ed00c2051d2950
MAB Sarker <chayan2005@gmail.com>
2025-03-20 10:00:05 -0400

    removed bkp fililes

--- electron-prayer-app/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron');
-
-// Expose safe timer functionality to the renderer process
-contextBridge.exposeInMainWorld('timerAPI', {
-    setInterval: (callback, delay) => setInterval(callback, delay),
-});
